452c43319f019ca286126c6fb14608de963901c4
jas4code <jasabocejo@protonmail.com>
2024-02-27 23:52:58 +0800

    Using Scroll Views

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import { View, Text, Image, ImageBackground } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
 	return (
 		<View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-			{/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
-			<Image source={{ uri: 'https://picsum.photos/300' }} style={{ width: 300, height: 300 }} /> */}
-
-			{/* Setting up a background image */}
-			<ImageBackground source={logoImg} style={{ flex: 1 }}>
-				<Text>IMAGE TEXT HERE</Text>
-			</ImageBackground>
+			<ScrollView>
+				<Image source={logoImg} style={{ width: 300, height: 300 }} />
+				<Text>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum magni illo in, incidunt eos aspernatur. Hic, expedita. Praesentium quis eaque sit et minus nihil dolore quasi, necessitatibus ipsa amet eum repudiandae repellendus dolorum in laudantium illum corporis, sequi tempora totam aperiam adipisci excepturi! Obcaecati esse praesentium consectetur nulla cupiditate atque repudiandae, voluptas eveniet itaque. Vitae blanditiis repellendus dolorum ex id velit et laborum voluptas architecto, illo error nam vero tempora, minima sit itaque odio incidunt delectus dolore? Sint quos vitae unde, fuga impedit iure cumque accusantium nobis, quia
+					vero architecto aspernatur? Iusto repellendus quidem eligendi atque, voluptatum officia eaque deserunt.
+				</Text>
+				<Image source={logoImg} style={{ width: 300, height: 300 }} />
+			</ScrollView>
 		</View>
 	);
 }
