9fb4eb145f62e0244c78c7904403bbb8704e4762
jas4code <jasabocejo@protonmail.com>
2024-02-28 02:18:50 +0800

    Alert API

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { View, ActivityIndicator } from 'react-native';
+import { View, Button, Alert } from 'react-native';
 
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
 	return (
 		<View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-			<ActivityIndicator size='large' />
-			<ActivityIndicator size='large' color='midnightblue' />
-			<ActivityIndicator size='large' color='midnightblue' animating={false} />
+			<Button title='Alert 1' onPress={() => Alert.alert('Invalid data!')} />
+			<Button title='Alert 2' onPress={() => Alert.alert('Invalid data!', 'DOB incorrect')} />
+			<Button
+				title='Alert 3'
+				onPress={() =>
+					Alert.alert('Invalid data!', 'DOB incorrect', [
+						{
+							text: 'Cancel',
+							onPress: () => console.log('Cancel pressed.'),
+						},
+						{
+							text: 'OK',
+							onPress: () => console.log('OK pressed.'),
+						},
+						{
+							text: 'Sample1',
+							onPress: () => console.log('Sample1 pressed.'),
+						},
+					])
+				}
+			/>
 		</View>
 	);
 }
