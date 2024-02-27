45bc0af4ddea7c0d8adc2e9551292fe2c4450b30
jas4code <jasabocejo@protonmail.com>
2024-02-28 02:57:51 +0800

    Custom Components

--- App.js
+++ App.js
@@ -1,31 +1,13 @@
-import { View, Button, Alert } from 'react-native';
+import { View } from 'react-native';
+import Greet from './components/Greet';
 
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
 	return (
 		<View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-			<Button title='Alert 1' onPress={() => Alert.alert('Invalid data!')} />
-			<Button title='Alert 2' onPress={() => Alert.alert('Invalid data!', 'DOB incorrect')} />
-			<Button
-				title='Alert 3'
-				onPress={() =>
-					Alert.alert('Invalid data!', 'DOB incorrect', [
-						{
-							text: 'Cancel',
-							onPress: () => console.log('Cancel pressed.'),
-						},
-						{
-							text: 'OK',
-							onPress: () => console.log('OK pressed.'),
-						},
-						{
-							text: 'Sample1',
-							onPress: () => console.log('Sample1 pressed.'),
-						},
-					])
-				}
-			/>
+			<Greet name='Bruce Wayne' />
+			<Greet name='Clark Kent' />
 		</View>
 	);
 }
