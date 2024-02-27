73397e8a497df6ada97c99e3cec3cdceb70bc276
jas4code <jasabocejo@protonmail.com>
2024-02-28 01:50:55 +0800

    Alert

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { View, StatusBar } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
 	return (
 		<View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-			<StatusBar backgroundColor='lightgreen' barStyle='light-content' hidden />
+			<ActivityIndicator size='large' />
+			<ActivityIndicator size='large' color='midnightblue' />
+			<ActivityIndicator size='large' color='midnightblue' animating={false} />
 		</View>
 	);
 }
