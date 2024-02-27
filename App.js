171e82595512314600f7288c3520bb5657051161
jas4code <jasabocejo@protonmail.com>
2024-02-28 01:32:53 +0800

    Status Bar

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { useState } from 'react';
-import { View, Text, Button, Modal } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
-	const [isModalVisible, setIsModalVisible] = useState(false);
-
 	return (
 		<View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-			<Button title='Open Modal' onPress={() => setIsModalVisible(true)} color='midnightblue' />
-
-			<Modal visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)} animationType='slide' presentationStyle='pageScreen'>
-				<View style={{ flex: 1, backgroundColor: 'lightblue', padding: 60 }}>
-					<Text>Modal content</Text>
-					<Button title='Close' color='midnightblue' onPress={() => setIsModalVisible(false)} />
-				</View>
-			</Modal>
+			<StatusBar backgroundColor='lightgreen' barStyle='light-content' hidden />
 		</View>
 	);
 }
