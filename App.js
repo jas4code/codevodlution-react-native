ab5c598058403a8b2bc4cb39b3b33c6292cbfdfd
jas4code <jasabocejo@protonmail.com>
2024-02-28 23:37:03 +0800

    StyleSheet API

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { View } from 'react-native';
-import Greet from './components/Greet';
-
-const logoImg = require('./assets/adaptive-icon.png');
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-			<Greet name='Bruce Wayne' />
-			<Greet name='Clark Kent' />
+		// <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
+		<View style={styles.container}>
+			<Text style={styles.title}>Styleshee API</Text>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'plum',
+		padding: 60,
+	},
+	title: {},
+});
