e1c8ac7220651910663af9d0b3b3aa712c6ca670
jas4code <jasabocejo@protonmail.com>
2024-02-28 23:59:03 +0800

    Using Multiplt StyleSheet

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ export default function App() {
 	return (
 		// <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
 		<View style={styles.container}>
-			<Text style={styles.title}>Styleshee API</Text>
+			<View style={[styles.box, styles.lightblueBg]}>
+				<Text>Lightblue box</Text>
+			</View>
+			<View style={[styles.box, styles.lightgreenBg]}>
+				<Text>Lightgreen box</Text>
+			</View>
 		</View>
 	);
 }
@@ -15,5 +20,16 @@ const styles = StyleSheet.create({
 		backgroundColor: 'plum',
 		padding: 60,
 	},
-	title: {},
+	box: {
+		width: 100,
+		height: 100,
+		padding: 10,
+		backgroundColor: 'pink',
+	},
+	lightblueBg: {
+		backgroundColor: 'lightblue',
+	},
+	lightgreenBg: {
+		backgroundColor: 'lightgreen',
+	},
 });
