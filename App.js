bd94e975e7fb11eb593a2776f0d4e1fc6be820cc
jas4code <jasabocejo@protonmail.com>
2024-02-29 02:01:05 +0800

    Understanding flex Property

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import Box from './components/Box';
 
 export default function App() {
 	return (
+		// <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
+
 		<View style={styles.container}>
-			<Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
-			<Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
+			<Box style={{ backgroundColor: '#8e9b00', flex: 1 }}>Box 1</Box>
+			<Box style={{ backgroundColor: '#b65d1f', flex: 3 }}>Box 2</Box>
 			<Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
 			<Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
 			<Box style={{ backgroundColor: '#1c4c56' }}>Box 5</Box>
@@ -16,6 +18,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
 	container: {
+		flex: 1,
 		marginTop: 64,
 		borderWidth: 6,
 		borderColor: 'red',
