104e09224fec5363ae2079792d88fb8f845fb797
jas4code <jasabocejo@protonmail.com>
2024-03-04 22:36:11 +0800

    Using flexBasis Property

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ export default function App() {
 		// <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
 
 		<View style={styles.container}>
-			<Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
-			<Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
-			<Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
-			<Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
+			<Box style={{ backgroundColor: '#8e9b00' }}>Box 1 shrink item</Box>
+			<Box style={{ backgroundColor: '#b65d1f' }}>Box 2 shrink item</Box>
+			<Box style={{ backgroundColor: '#1c4c56', flexBasis: 140, flex: 1 }}>Box 3</Box>
+			<Box style={{ backgroundColor: '#ab9156', height: 140, flex: 1 }}>Box 4</Box>
 			<Box style={{ backgroundColor: '#1c4c56' }}>Box 5</Box>
 			<Box style={{ backgroundColor: '#b95f21' }}>Box 6</Box>
+			<Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box>
 		</View>
 	);
 }
@@ -23,11 +24,6 @@ const styles = StyleSheet.create({
 		borderWidth: 6,
 		borderColor: 'red',
 
-		// flex: 1,
-		height: 300,
-		flexWrap: 'wrap',
-		// rowGap: 20,
-		// columnGap: 20,
-		gap: 10,
+		flex: 1,
 	},
 });
