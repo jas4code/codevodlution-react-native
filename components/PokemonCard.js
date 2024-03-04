f83eaa019c3ef441a373515944ce70bd763c672c
jas4code <jasabocejo@protonmail.com>
2024-03-05 02:37:35 +0800

    Setting Up Code for Pokemon Card Exercise

---
+++ components/PokemonCard.js
@@ -0,0 +1,10 @@
+import {  Text, View } from 'react-native'
+import React from 'react'
+
+export default function PokemonCard() {
+  return (
+    <View>
+      <Text>Pokemon Card</Text>
+    </View>
+  )
+}
