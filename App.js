2befb03492297974b856ed2d67f6df159a33c4d3
jas4code <jasabocejo@protonmail.com>
2024-03-04 23:01:03 +0800

    Using flexShrink Property

--- App.js
+++ App.js
@@ -6,13 +6,13 @@ export default function App() {
 		// <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
 
 		<View style={styles.container}>
-			<Box style={{ backgroundColor: '#8e9b00' }}>Box 1 shrink item</Box>
-			<Box style={{ backgroundColor: '#b65d1f' }}>Box 2 shrink item</Box>
-			<Box style={{ backgroundColor: '#1c4c56', flexBasis: 140, flex: 1 }}>Box 3</Box>
-			<Box style={{ backgroundColor: '#ab9156', height: 140, flex: 1 }}>Box 4</Box>
-			<Box style={{ backgroundColor: '#1c4c56' }}>Box 5</Box>
-			<Box style={{ backgroundColor: '#b95f21' }}>Box 6</Box>
-			<Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box>
+			<Box style={{ backgroundColor: '#8e9b00', flexShrink: 1 }}>Box 1 shrink</Box>
+			<Box style={{ backgroundColor: '#b65d1f', flexShrink: 2 }}>Box 2 shrink</Box>
+			{/* <Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
+			<Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
+			<Box style={{ backgroundColor: '#6b0803' }}>Box 5</Box>
+			<Box style={{ backgroundColor: '#1c4c56' }}>Box 6</Box>
+			<Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box> */}
 		</View>
 	);
 }
@@ -25,5 +25,8 @@ const styles = StyleSheet.create({
 		borderColor: 'red',
 
 		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'flex-start',
+		width: 300,
 	},
 });
