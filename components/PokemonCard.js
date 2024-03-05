b89eea50d2ddb2b173a4ffee0bf3981b69b254b1
jas4code <jasabocejo@protonmail.com>
2024-03-06 01:22:45 +0800

    Styling the Card Component

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,10 +1,31 @@
-import {  Text, View } from 'react-native'
-import React from 'react'
+import { Text, View, StyleSheet, Platform } from 'react-native';
+import React from 'react';
 
 export default function PokemonCard() {
-  return (
-    <View>
-      <Text>Pokemon Card</Text>
-    </View>
-  )
+	return (
+		<View style={styles.card}>
+			<Text>Pokemon Card</Text>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	card: {
+		backgroundColor: '#fff',
+		borderRadius: 16,
+		borderWidth: 2,
+		padding: 16,
+		margin: 16,
+		...Platform.select({
+			ios: {
+				shadowOffset: { width: 2, height: 2 },
+				shadowColor: '#333',
+				shadowOpacity: 0.3,
+				shadowRadius: 4,
+			},
+			android: {
+				elevation: 5,
+			},
+		}),
+	},
+});
