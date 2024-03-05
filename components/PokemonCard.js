ab5a8be3b9bd7b350544829a783d77243265e9e1
jas4code <jasabocejo@protonmail.com>
2024-03-06 02:30:05 +0800

    Styling Card Content-2

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,7 +1,24 @@
 import { Text, View, StyleSheet, Platform, Image } from 'react-native';
 import React from 'react';
 
+const getTypeDetails = (type) => {
+	switch (type.toLowerCase()) {
+		case 'electric':
+			return { borderColor: '#FFD700', emoji: '⚡' };
+		case 'water':
+			return { borderColor: '#6493EA', emoji: '💧' };
+		case 'fire':
+			return { borderColor: '#FF5733', emoji: '🔥' };
+		case 'grass':
+			return { borderColor: '#66CC66', emoji: '🌿' };
+		default:
+			return { borderColor: '#A0A0A0', emoji: '❓' };
+	}
+};
+
 export default function PokemonCard({ name, image, type, hp, moves, weaknesses }) {
+	const { borderColor, emoji } = getTypeDetails(type);
+
 	return (
 		<View style={styles.card}>
 			<View style={styles.nameContainer}>
@@ -11,16 +28,19 @@ export default function PokemonCard({ name, image, type, hp, moves, weaknesses }
 
 			<Image source={image} style={styles.image} accessibilityLabel={`${name} pokemon`} resizeMode='contain' />
 
-			<View>
-				<Text>{type}</Text>
+			<View style={styles.typeContainer}>
+				<View style={[styles.badge, { borderColor }]}>
+					<Text style={styles.typeEmoji}>{emoji}</Text>
+					<Text style={styles.typeText}>{type}</Text>
+				</View>
 			</View>
 
-			<View>
-				<Text>Moves: {moves.join(', ')}</Text>
+			<View style={styles.movesContainer}>
+				<Text style={styles.movesText}>Moves: {moves.join(', ')}</Text>
 			</View>
 
-			<View>
-				<Text>Weaknesses: {weaknesses.join(', ')}</Text>
+			<View style={styles.weaknessContainer}>
+				<Text style={styles.weaknessText}>Weaknesses: {weaknesses.join(', ')}</Text>
 			</View>
 		</View>
 	);
@@ -61,5 +81,39 @@ const styles = StyleSheet.create({
 		width: '100%',
 		height: 200,
 		marginBottom: 16,
+	},
+	typeContainer: {
+		alignItems: 'center',
+		marginBottom: 40,
+	},
+	badge: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingVerticalL: 6,
+		paddingHorizontal: 12,
+		borderRadius: 20,
+		borderWidth: 4,
+	},
+	typeEmoji: {
+		fontSize: 30,
+		marginRight: 12,
+	},
+	typeText: {
+		fontSize: 22,
+		fontWeight: 'bold',
+	},
+	movesContainer: {
+		marginBottom: 6,
+	},
+	movesText: {
+		fontSize: 22,
+		fontWeight: 'bold',
+	},
+	weaknessContainer: {
+		marginBottom: 8,
+	},
+	weaknessText: {
+		fontSize: 22,
+		fontWeight: 'bold',
 	},
 });
