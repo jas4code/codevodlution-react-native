b723d352ac56337f7fa717e43bdda9286b4eb553
jas4code <jasabocejo@protonmail.com>
2024-03-09 12:13:09 +0800

    Introduction to FlatList

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View, ScrollView, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, StatusBar, FlatList } from 'react-native';
 import pokemonList from './data.json';
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<ScrollView style={styles.scrollView}>
+			{/* <ScrollView style={styles.scrollView}>
 				{pokemonList.map((pokemon) => {
+					console.log(pokemon.id);
 					return (
 						<View style={styles.card} key={pokemon.id}>
 							<Text style={styles.cardText}>{pokemon.type}</Text>
@@ -13,7 +14,23 @@ export default function App() {
 						</View>
 					);
 				})}
-			</ScrollView>
+			</ScrollView> */}
+
+			<FlatList
+				style={styles.flatList}
+				data={pokemonList}
+				renderItem={({ item }) => {
+					// console.log(item.id);
+					return (
+						<View style={styles.card} key={item.id}>
+							<Text style={styles.cardText}>{item.type}</Text>
+							<Text style={styles.cardText}>{item.name}</Text>
+						</View>
+					);
+				}}
+				// horizontal
+				keyExtractor={(item) => item.id.toString()}
+			/>
 		</View>
 	);
 }
@@ -24,9 +41,14 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		paddingTop: StatusBar.currentHeight,
 	},
-	scrollView: {
+
+	// scrollView: {
+	// 	paddingHorizontal: 16,
+	// },
+	flatList: {
 		paddingHorizontal: 16,
 	},
+
 	card: {
 		backgroundColor: 'white',
 		padding: 16,
