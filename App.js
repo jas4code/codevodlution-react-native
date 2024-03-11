bf5bb0afb517f464f151f6f0ba473154db3e8a09
jas4code <jasabocejo@protonmail.com>
2024-03-12 07:45:40 +0800

    Setting up Code for Login Form Exercise

--- App.js
+++ App.js
@@ -1,35 +1,54 @@
-import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
-	const [name, setName] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
 	return (
-		<SafeAreaView style={styles.container}>
-			<TextInput style={styles.input} value={name} onChangeText={setName} placeholder='email@example.com' autoCapitalize='none' autoCorrect={false} secureTextEntry keyboardType='default' />
-			<TextInput style={[styles.input, styles.multilineText]} placeholder='message' multiline />
-			<Text style={styles.text}>My name is {name}!</Text>
-		</SafeAreaView>
+		<View style={styles.container}>
+			<View style={styles.form}>
+				<Text style={styles.label}>Username</Text>
+				<TextInput style={styles.input} placeholder='Enter your username' value={username} onChangeText={setUsername} />
+				<Text style={styles.label}>Password</Text>
+				<TextInput style={styles.input} placeholder='Enter your password' secureTextEntry value={password} onChangeText={setPassword} />
+				<Button title='login' onPress={() => {}} />
+			</View>
+		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
-		paddingTop: StatusBar.currentHeight,
+		justifyContent: 'center',
+		paddingHorizontal: 20,
+		backgroundColor: '#f5f5f5',
+	},
+	form: {
+		backgroundColor: 'white',
+		padding: 20,
+		borderRadius: 10,
+		shadowColor: 'black',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+	label: {
+		fontSize: 16,
+		marginBottom: 5,
+		fontWeight: 'bold',
 	},
 	input: {
 		height: 40,
-		margin: 12,
-		padding: 10,
+		borderColor: '#ddd',
 		borderWidth: 1,
-	},
-	text: {
-		fontSize: 24,
-	},
-	multilineText: {
-		minHeight: 100,
-		textAlignVertical: 'top',
+		marginBottom: 15,
+		padding: 10,
+		borderRadius: 5,
 	},
 });
