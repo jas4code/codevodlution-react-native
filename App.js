ed55a8cd1ff48f30e7b283e32fd70bb4b5e67334
jas4code <jasabocejo@protonmail.com>
2024-03-16 11:38:43 +0800

    Using GET Request from an API
    and setting up the code for the layout

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar, StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [postList, setPostList] = useState([]);
+
+	const fetchData = async (limit = 10) => {
+		const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
+		const data = await response.json();
+		setPostList(data);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={StyleSheet.listContainer}>
+				<FlatList
+					data={postList}
+					renderItem={({ item }) => {
+						return (
+							<View style={styles.card}>
+								<Text style={styles.titleText}>{item.title}</Text>
+								<Text style={styles.bodyText}>{item.body}</Text>
+							</View>
+						);
+					}}
+					ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+					ListEmptyComponent={<Text>No Posts Found</Text>}
+					ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
+					ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
+				/>
+			</View>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		paddingTop: StatusBar.currentHeight,
+	},
+	listContainer: {
+		flex: 1,
+		paddingHorizontal: 16,
+	},
+	card: {
+		backgroundColor: 'white',
+		padding: 16,
+		borderRadius: 8,
+		borderWidth: 8,
+		borderWidth: 1,
+	},
+	titleText: {
+		fontSize: 24,
+	},
+	bodyText: {
+		fontSize: 18,
+		color: '#666666',
+	},
+	headerText: {
+		fontSize: 24,
+		textAlign: 'center',
+		marginBottom: 12,
+	},
+	footerText: {
+		fontSize: 24,
+		textAlign: 'center',
+		marginTop: 12,
+	},
 });
