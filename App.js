6b19d8d32b73a4bb01b0260a2d4c7b3049e0cbd3
jas4code <jasabocejo@protonmail.com>
2024-03-16 14:42:08 +0800

    Using POST Request from RN App

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { Text, View, StatusBar, StyleSheet, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
+import { Text, View, StatusBar, StyleSheet, SafeAreaView, FlatList, ActivityIndicator, TextInput, Button } from 'react-native';
 import { useEffect, useState } from 'react';
 
 export default function App() {
 	const [postList, setPostList] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [refreshing, setRefreshing] = useState(false);
+	const [postTitle, setPostTitle] = useState('');
+	const [postBody, setPostBody] = useState('');
+	const [isPosting, setIsPosting] = useState(false);
 
 	const fetchData = async (limit = 10) => {
 		const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
@@ -17,6 +20,25 @@ export default function App() {
 		setRefreshing(true);
 		fetchData(20);
 		setRefreshing(false);
+	};
+
+	const addPost = async () => {
+		setIsPosting(true);
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				title: postTitle,
+				body: postBody,
+			}),
+		});
+		const newPost = await response.json();
+		setPostList([newPost, ...postList]);
+		setPostTitle('');
+		setPostBody('');
+		setIsPosting(false);
 	};
 
 	useEffect(() => {
@@ -34,27 +56,34 @@ export default function App() {
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<View style={StyleSheet.listContainer}>
-				<FlatList
-					data={postList}
-					renderItem={({ item }) => {
-						return (
-							<View style={styles.card}>
-								<Text style={styles.titleText}>
-									<Text style={{ fontWeight: 'bold' }}>{item.id})</Text> {item.title}
-								</Text>
-								<Text style={styles.bodyText}>{item.body}</Text>
-							</View>
-						);
-					}}
-					ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
-					ListEmptyComponent={<Text>No Posts Found</Text>}
-					ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
-					ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
-					refreshing={refreshing}
-					onRefresh={handleRefresh}
-				/>
-			</View>
+			<>
+				<View style={styles.inputContainer}>
+					<TextInput style={styles.input} placeholder='Post Title' value={postTitle} onChangeText={setPostTitle} />
+					<TextInput style={styles.input} placeholder='Post Body' value={postBody} onChangeText={setPostBody} />
+					<Button title={isPosting ? 'Adding...' : 'Add Post'} onPress={addPost} disabled={isPosting} />
+				</View>
+				<View style={styles.listContainer}>
+					<FlatList
+						data={postList}
+						renderItem={({ item }) => {
+							return (
+								<View style={styles.card}>
+									<Text style={styles.titleText}>
+										<Text style={{ fontWeight: 'bold' }}>{item.id})</Text> {item.title}
+									</Text>
+									<Text style={styles.bodyText}>{item.body}</Text>
+								</View>
+							);
+						}}
+						ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+						ListEmptyComponent={<Text>No Posts Found</Text>}
+						ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
+						ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
+						refreshing={refreshing}
+						onRefresh={handleRefresh}
+					/>
+				</View>
+			</>
 		</SafeAreaView>
 	);
 }
@@ -67,7 +96,7 @@ const styles = StyleSheet.create({
 	},
 	listContainer: {
 		flex: 1,
-		paddingHorizontal: 16,
+		paddingHorizontal: 12,
 	},
 	card: {
 		backgroundColor: 'white',
@@ -99,5 +128,20 @@ const styles = StyleSheet.create({
 		paddingTop: StatusBar.currentHeight,
 		justifyContent: 'center',
 		alignItems: 'center',
+	},
+	inputContainer: {
+		backgroundColor: 'white',
+		padding: 16,
+		borderRadius: 8,
+		borderWidth: 1,
+		margin: 16,
+	},
+	input: {
+		height: 48,
+		borderColor: 'gray',
+		borderWidth: 1,
+		marginBottom: 8,
+		padding: 8,
+		borderRadius: 8,
 	},
 });
