fbccd59ac094e5a883ce3a78e6828470b60339ff
jas4code <jasabocejo@protonmail.com>
2024-03-16 13:15:48 +0800

    Using Pul-to-Refresh Functionality

--- App.js
+++ App.js
@@ -4,12 +4,19 @@ import { useEffect, useState } from 'react';
 export default function App() {
 	const [postList, setPostList] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [refreshing, setRefreshing] = useState(false);
 
 	const fetchData = async (limit = 10) => {
 		const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
 		const data = await response.json();
 		setPostList(data);
-		// setIsLoading(false);
+		setIsLoading(false);
+	};
+
+	const handleRefresh = () => {
+		setRefreshing(true);
+		fetchData(20);
+		setRefreshing(false);
 	};
 
 	useEffect(() => {
@@ -33,7 +40,9 @@ export default function App() {
 					renderItem={({ item }) => {
 						return (
 							<View style={styles.card}>
-								<Text style={styles.titleText}>{item.title}</Text>
+								<Text style={styles.titleText}>
+									<Text style={{ fontWeight: 'bold' }}>{item.id})</Text> {item.title}
+								</Text>
 								<Text style={styles.bodyText}>{item.body}</Text>
 							</View>
 						);
@@ -42,6 +51,8 @@ export default function App() {
 					ListEmptyComponent={<Text>No Posts Found</Text>}
 					ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
 					ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
+					refreshing={refreshing}
+					onRefresh={handleRefresh}
 				/>
 			</View>
 		</SafeAreaView>
